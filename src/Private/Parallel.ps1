fd6edc58ad2f8f0e27c3f734d392d27980f13cf6
Tony Nash <tony.nash@intecbusiness.co.uk>
2025-11-11 14:42:31 +0000

    updated version (keeps IIS/Hyper-V/SMB/etc. sections, adds Printers, MUs, Findings, Confidence; trims large tables to keep HTML snappy)

--- src/Private/Parallel.ps1
+++ src/Private/Parallel.ps1
@@ -1,19 +1,30 @@
 function Invoke-RunspaceTasks {
   [CmdletBinding()]
-  param(
-    [Parameter(Mandatory)] [array]$Tasks,  # @{Name; ScriptBlock; Args}
-    [int]$Throttle = [Environment]::ProcessorCount
-  )
+  param([Parameter(Mandatory)] [array]$Tasks, [int]$Throttle = [Environment]::ProcessorCount)
+
   Add-Type -AssemblyName System.Management.Automation
-  $pool = [runspacefactory]::CreateRunspacePool(1,$Throttle)
+  $iss = [System.Management.Automation.Runspaces.InitialSessionState]::CreateDefault()
+  $mod = Join-Path $PSScriptRoot '..\ServerAuditToolkitV2.psd1'
+  $iss.ImportPSModule($mod)
+  $pool = [runspacefactory]::CreateRunspacePool(1,$Throttle,$iss,$host)
+  $pool.ApartmentState = 'MTA'
   $pool.Open()
+
+  $worker = {
+    param($fn,$targets,$cap,$verbosePref)
+    & $fn -ComputerName $targets -Capability $cap -Verbose:$verbosePref
+  }.ToString()
+
   $handles = @()
   foreach ($t in $Tasks) {
-    $ps = [powershell]::Create().AddScript($t.ScriptBlock).AddArgument($t.Name)
-    $t.Args | ForEach-Object { $ps.AddArgument($_) | Out-Null }
+    $ps = [powershell]::Create()
     $ps.RunspacePool = $pool
+    $null = $ps.AddScript($worker).AddArgument($t.Name)
+    foreach ($a in $t.Args) { $null = $ps.AddArgument($a) }
+    $null = $ps.AddArgument($VerbosePreference)
     $handles += [pscustomobject]@{ Name=$t.Name; PS=$ps; Handle=$ps.BeginInvoke() }
   }
+
   $out = @()
   foreach ($h in $handles) {
     $data = $h.PS.EndInvoke($h.Handle)
@@ -21,15 +32,5 @@ function Invoke-RunspaceTasks {
     $h.PS.Dispose()
   }
   $pool.Close(); $pool.Dispose()
-  return $out
-}
-
-function Invoke-Serial {
-  param([array]$Tasks)
-  $out=@()
-  foreach ($t in $Tasks) {
-    $data = & $t.ScriptBlock @($t.Name + $t.Args)
-    $out += [pscustomobject]@{ Name=$t.Name; Data=$data }
-  }
   return $out
 }
